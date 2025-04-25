c8da3c4ceec3ec051a0da776ebb7eb00844f90d3
deepakreddy0204 <143284285+deepakreddy0204@users.noreply.github.com>
2025-04-25 18:54:45 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,12 @@
 <?php
 
-    $con = mysqli_connect("127.0.0.1","root","","mr");
+    $con = mysqli_connect("thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "c52rgdym3wse9u6y", "m3yyz6fsg0fdvzuh", "i54zqn1gz5e95xrv");
+
     // Check connection
-    if (mysqli_connect_errno()){
+    if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    } else {
+        echo "Connected successfully!";
     }
+
 ?>
