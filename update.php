ffa1eb90c0d02d81c7b10f99429ced40a532acfe
deepakreddy0204 <143284285+deepakreddy0204@users.noreply.github.com>
2025-04-30 11:21:49 -0500

    Update update.php

--- update.php
+++ update.php
@@ -6,6 +6,6 @@
 	mysqli_query($con,"update `maintenance_remainder` set task='$task', due_date='$due_date' where remainder_id='$remainder_id'");
 	echo "<div class='form'>
     <h3>Updated succesfully</h3><br/>
-    <p class='link'>Click here to see items<a href='Home.php'>Home</a></p>
+    <p class='link'>Click here to see items<a href='home.php'>Home</a></p>
     </div>";
 ?>
